94baf245cd91222d9bb74a9ada6000f27f2345bf
Shahrukh Malik <shahrukhm@hsco.pk>
2023-09-07 00:05:23 +0300

    initialized React app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://dervaish.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dervaish
+        </a>
+        <p>
+          Insights in Faith
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
