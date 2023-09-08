c125ffbda26dd7fabeff0b6e53afa98ab38c1320
Shahrukh Malik <shahrukhm@hsco.pk>
2023-09-09 02:20:21 +0300

    fixed reactplayer to only used file

--- src/components/MediaPlayer.jsx
+++ src/components/MediaPlayer.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import ReactPlayer from 'react-player';
+import ReactPlayer from 'react-player/file';
 import axios from 'axios'; // Import Axios
 
 import './MediaPlayer.css';
